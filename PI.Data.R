b08dde989e70e7e8c65af3db8be9405e3173519c
iwargowsky <iwargowsky@woodwellclimate.org>
2023-10-13 10:21:45 -0400

    added new data

--- PI.Data.R
+++ PI.Data.R
@@ -62,12 +62,24 @@ roy <- read_csv("ABCflux_MES.csv")
 hung.ec <- read_csv("ABCfluxv2_Hung_tower.csv")
 hung.ch <- read_csv("ABCfluxv2_Hung_chamber.csv")
 
+### Liam Heffernan ####---------------------------------------------------------
+heffernan <- read_csv("ABCfluxv2.vars.liamheffernan.lutose.csv")
+
+### Helena Rautakoski###--------------------------------------------------------
+rautakoski <- read_csv("ABCfluxv2_Ranskala.csv")
+
+### Efren Lopez-Blanco###-------------------------------------------------------
+lopezblanco <- read_csv("ABCflux_GEM2022data.csv", na = "-9999")
+
+
+
+## PROCESSING DATA FROM PIs #############################################################################
 ### Scott Davidson####----------------------------------------------------------
-davidson.16 <- read_excel("Davidson/Davidson_et al. 2016 Ecosystems.xlsx", sheet= 2)
 #####Processing davidson.16####
+davidson.16 <- read_excel("Davidson/Davidson_et al. 2016 Ecosystems.xlsx", sheet= 2)
 davidson.16.monthly <- davidson.16 %>% 
   mutate(year= substr(date, 1,4), month= substr(date,5,6), day= substr(date,7,8)) %>%
-  group_by(year, month, site_position ) %>%
+  group_by(year, month, site_position, wetness, vegetation, position) %>%
   summarise(tsoil_surface= mean(`temp_10 (deg C)`, na.rm = TRUE) ,
             thaw_depth= mean(`thaw depth  (cm)`, na.rm = TRUE),
             gpp= mean(`GPP (gC - CO2 m2 hr1)`, na.rm = TRUE),
@@ -84,8 +96,11 @@ davidson.16.monthly$nee <- davidson.16.monthly$nee *24* days_in_month(as.yearmon
 davidson.16.monthly$gpp <- davidson.16.monthly$gpp *24* days_in_month(as.yearmon(paste(davidson.16.monthly$year,davidson.16.monthly$month,sep = '-')))
 davidson.16.monthly$reco <- davidson.16.monthly$reco *24* days_in_month(as.yearmon(paste(davidson.16.monthly$year,davidson.16.monthly$month,sep = '-')))
 davidson.16.monthly$ch4_flux_total <- davidson.16.monthly$ch4_flux_total /1000*24* days_in_month(as.yearmon(paste(davidson.16.monthly$year,davidson.16.monthly$month,sep = '-')))
+davidson.16.monthly$water_table_depth <- davidson.16.monthly$water_table_depth *-1
 #rename column
-davidson.16.monthly <- davidson.16.monthly %>% dplyr::rename("site_reference"="site_position")
+davidson.16.monthly <- davidson.16.monthly %>% dplyr::rename("site_reference"="site_position",
+                                                             "soil_moisture_class"= "wetness",
+                                                             "veg_detail"= "vegetation")
 #Adding static info
 davidson.16.monthly$tsoil_surface_depth <- "10"
 davidson.16.monthly$data_contributor_or_author <- "Scott Davidson"
@@ -98,6 +113,7 @@ davidson.16.monthly$flux_method <- "Chamber"
 davidson.16.monthly$flux_method_detail <-  "Manual Chamber"
 davidson.16.monthly$flux_method_description <- "PVC collars (height 15 cm 9 diameter 20 cm). The UGGA was used to measure both CH4 and CO2 concentrations using a closed system with a 1 Hz sampling rate. The chamber was left in place for two minutes to achieve a stable increase in CH4 and CO2 concentration within the chamber headspace. NEE was measured with transparent chamber and ER was measured with opaque chamber (hood) that is used to calculate GPP (GPP = NEE + ER)"
 davidson.16.monthly$instrumentation <- "LGRTM, Ultraportable Greenhouse Gas Analyser (UGGA), Model 915-0011 (Los Gatos, Research, Palo Alto, CA, USA)"
+davidson.16.monthly$diurnal_coverage <- "Day"
 davidson.16.monthly <- davidson.16.monthly %>% 
   mutate(site_name = case_when(site_reference %in% c("BEO_flat_centre","BEO_high_centre", "BEO_low_centre",
                                                      "BEO_rim", "BEO_trough")~ "Barrow-BEO",
@@ -127,6 +143,11 @@ davidson.16.monthly <- davidson.16.monthly %>%
   mutate(permafrost= case_when(site_name %in% c("Barrow-BEO","Barrow-BES")~ "Unknown",
                                site_name %in% "Atqasuk"~ "Yes",
                                site_name %in% "Ivotuk"~ "No"))
+davidson.16.monthly <- davidson.16.monthly %>% 
+  mutate(landform= case_when(site_name %in% "Barrow-BEO"~ "Well-developed high-centre, flat- centre and low-centre polygons",
+                             site_name %in% "Barrow-BES"~ "Drained thaw lake basin with a modest development of low centre polygon",
+                             site_name %in% "Atqasuk"~ "Well-developed, low-centred, ice- wedge polygons with well-drained rims",
+                             site_name %in% "Ivotuk"~ "Lacks substantial ice-wedge polygon development and comprises a gentle north-west facing slope and a lower lying wet meadow on the margins of a stream"))
 davidson.16.monthly <- davidson.16.monthly %>% 
   mutate(dec_shrub = case_when(site_reference %in% c("ATQ_ridge_tussock", "ATQ_ridge_inter_tussock",
                                "IVO_plateau_inter_tussock", "IVO_wetland")~ "Present",
@@ -163,41 +184,41 @@ davidson.16.monthly <- davidson.16.monthly %>%
                                       "IVO_plateau_inter_tussock","IVO_plateau_hollow", "IVO_wetland") ~ "Present",
                                       site_reference %in% c("ATQ_pool","BES_drained_lake" ,"BEO_rim")~ "Absent"))
 davidson.16.monthly <- davidson.16.monthly %>% 
-  mutate(veg_detail = case_when(site_reference %in% c("BEO_flat_centre","BEO_rim")~ "Dry graminoid",
-                               site_reference %in% "BEO_high_centre" ~ "Moss-lichen",
-                               site_reference %in% c("BEO_low_centre", "BEO_trough", "BES_drained_lake",
-                                                     "IVO_wetland" , "ATQ_pool") ~ "Wet sedge",
-                               site_reference %in% "IVO_plateau_hollow" ~ "Moss only",
-                               site_reference %in% c("IVO_plateau_inter_tussock", "ATQ_ridge_inter_tussock")~ "Moss-shrub",
-                               site_reference %in% c("IVO_plateau_tussock", "ATQ_ridge_tussock")~ "Tussock sedge"))
-davidson.16.monthly <- davidson.16.monthly %>% 
-  mutate(veg_detail = case_when(site_reference %in% "BEO_flat_centre"~ "UpTundra",
+  mutate(land_cover_bawld = case_when(site_reference %in% "BEO_flat_centre"~ "UpTundra",
                                 site_reference %in% c("BEO_high_centre","BEO_rim", "IVO_plateau_hollow",
                                                       "IVO_plateau_tussock","ATQ_ridge_inter_tussock",
                                                       "ATQ_ridge_tussock","IVO_plateau_inter_tussock" ) ~ "PermBog",
                                 site_reference %in% c("BEO_low_centre", "BEO_trough", "BES_drained_lake",
                                                       "IVO_wetland") ~ "PermWet",
                                 site_reference %in% "ATQ_pool"~ "Marshes"))
+#davidson.16.monthly <- davidson.16.monthly %>% 
+  #mutate(land_cover= case_when(site_name %in% "Barrow-BEO"~ "",
+                            # site_name %in% "Barrow-BES"~ "",
+                            # site_name %in% "Atqasuk"~ "",
+                            # site_name %in% "Ivotuk"~ ""))
 davidson.16.monthly$site_id <- paste("Davidson_",davidson.16.monthly$site_reference,"_agg", sep = "")
-#####
-davidson.19 <- read_excel("Davidson/Davidson et al. 2019 Biogeosciences_data.xlsx", sheet= 2)
+
 #####Processing davidson.19####
+davidson.19 <- read_excel("Davidson/Davidson et al. 2019 Biogeosciences_data.xlsx", sheet= 2)
 davidson.19.monthly <- davidson.19 %>% 
   mutate(year= year(as.Date(Date)),
          month= month(as.Date(Date)),
          day= day(as.Date(Date)),
          site_reference= paste(Microform, Burn, sep="_")) %>%
-  group_by(year, month, site_reference, Site, ) %>%
+  group_by(year, month, site_reference, Site,`Peatland type` ) %>%
   summarise(ch4_flux_total= mean(`mg CH4 m2 d1`, na.rm = TRUE),
             water_table_depth= mean(`Water table (cm) below ground surface`, na.rm = TRUE),
             chamber_nr_measurement_days= n_distinct(day))
 #change units
 davidson.19.monthly$ch4_flux_total <- davidson.19.monthly$ch4_flux_total /1000* days_in_month(as.yearmon(paste(davidson.19.monthly$year,davidson.19.monthly$month,sep = '-')))
+#rename columns
+davidson.19.monthly <- davidson.19.monthly %>% dplyr::rename('site_name'='Site',
+                                                             'veg_detail'='Peatland type')
 #adding in static info
 davidson.19.monthly$site_name <- "Poplar Fen"
 davidson.19.monthly$latitude <- "56.938833"
 davidson.19.monthly$longitude <- "-111.5489"
-davidson.19.monthly$site_id <- paste("PoplarFen", davidson.19.monthly$site_reference, "agg", sep = "_")
+davidson.19.monthly$site_id <- paste("Davidson_PoplarFen", davidson.19.monthly$site_reference, "agg", sep = "_")
 davidson.19.monthly$email <- "scott.davidson@plymouth.ac.uk"
 davidson.19.monthly$data_contributor_or_author <- "Scott Davidson"
 davidson.19.monthly$citation <- "https://doi.org/10.5194/bg-16-2651-2019"
@@ -215,16 +236,72 @@ davidson.19.monthly$flux_method <- "Chamber"
 davidson.19.monthly$flux_method_detail <- "Closed chambers"
 davidson.19.monthly$flux_method_description <- "A cylindrical opaque chamber (20 cm × 50 cm) was placed on the collar, with water poured around the collar edge to create a seal. A battery-powered fan was used to mix the chamber headspace. A thermocouple located within the chamber, attached to a thermometer, was used to measure temperature during sampling. A 20 mL syringe was used to collect gas samples at intervals of 7, 15, 25, and 35 min following chamber closure and injected into Exetainers (Labco, UK). A gas chromatograph with a flame ionization detector (250 ◦ C), helium gas carrier, and standards of 5 and 50 ppm was used to determine CH4 concentration"
 davidson.19.monthly$instrumentation <- "Shimadzu GC2014, Mandel Scientific"
-#####
+davidson.19.monthly$diurnal_coverage <- "Day"
+
+#####Processing davidson.21####
 davidson.21 <- read_excel("Davidson/Davidson_et al. 2021 JGR Biogeosciences.xlsx", sheet= 2)
-#####Processing data
+davidson.21 <- davidson.21 %>% mutate(year= year(as.Date(Date, format= "%m/%d/%Y")),
+                                      month= month(as.Date(Date, format= "%m/%d/%Y")),
+                                      day= day(as.Date(Date, format= "%m/%d/%Y")),
+                                      site_reference= paste(Peatland, Position, Veg_type, sep='_'))
+davidson.21.monthly <- davidson.21 %>% group_by(Site, site_reference, year, month ) %>%
+  group_by(year, month, Site, site_reference, Veg_type ) %>%
+  summarise(gpp= mean(GPP, na.rm=TRUE),
+            chamber_nr_measurement_days= n_distinct(day))
+#change unit
+davidson.21.monthly$gpp <- davidson.21.monthly$gpp *days_in_month(as.yearmon(paste(davidson.21.monthly$year,davidson.21.monthly$month,sep = '-')))
+#rename
+davidson.21.monthly <- davidson.21.monthly %>% dplyr::rename('site_name'= "Site")
+#adding in static info
+davidson.21.monthly$site_id <- paste("Davidson", davidson.21.monthly$site_name, davidson.21.monthly$site_reference, "agg", sep="_")
+davidson.21.monthly$data_contributor_or_author <- "Scott Davidson"
+davidson.21.monthly$email <- "scott.davidson@plymouth.ac.uk"
+davidson.21.monthly$citation <- "https://doi.org/10.1029/2021JG006403"
+davidson.21.monthly$country <- "Canada"
+davidson.21.monthly$gap_fill <- "Average"
+davidson.21.monthly$flux_method <- "Chamber"
+davidson.21.monthly$flux_method_detail <- "Closed dynamic chamber method"
+davidson.21.monthly$flux_method_description <- "A clear acrylic chamber (60 × 60 × 30 cm) was placed on a stainless-steel collar (60 × 60 cm).The concentration of CO2 (ppm) was determined inside the chamber at 15 s intervals for a maximum of 2.5 min. The linear change in CO2 concentration over time was used to calcu- late net ecosystem exchange (NEE; g CO2 m2 d−1). Ecosystem respiration (ER; g CO2 m2 d−1) was determined by darkening the chamber with an opaque cloth shroud. Gross primary production (GPP; g CO2 m2 d−1) was calculated as the difference between NEE and ER."
+davidson.21.monthly$instrumentation <- "portable infrared gas analyzer (EGM-4, PP systems)"
+davidson.21.monthly$diurnal_coverage <- "Day"
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(latitude= case_when(site_name %in% c("Carmon Creek", "Carmon Creek 2")~ "56.36222",
+                             site_name %in% "IPAD"~ "56.397561"),
+         longitude= case_when(site_name %in% c("Carmon Creek", "Carmon Creek 2")~ "-116.795833",
+                             site_name %in% "IPAD"~ "-116.891"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(disturbance= case_when(site_name %in% c("Carmon Creek", "Carmon Creek 2")~ "Seismic lines- approximately 3 m wide and run both north to south and east to west",
+                                site_name %in% "IPAD"~ "Seismic lines- approximately 7 m wide, running both southwest to northeast and east to west"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(disturb_year = case_when(site_name %in% c("Carmon Creek", "Carmon Creek 2")~ "~2003",
+                                site_name %in% "IPAD"~ "~2019"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(land_cover_bawld = case_when(site_name %in% c("Carmon Creek", "Carmon Creek 2")~ "Bog",
+                                  site_name %in% "IPAD"~ "Fen"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(veg_detail2 = case_when(site_name %in% c("Carmon Creek", "Carmon Creek 2")~ "Treed bog",
+                                site_name %in% "IPAD"~"Treed poor fen")) %>%
+  mutate(veg_detail= paste(veg_detail2, Veg_type), veg_detail2 = NULL, Veg_type= NULL)
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(ev_shrub = case_when(veg_detail %in% c("Treed bog Lichen/Rhododendron", "Treed bog Sphagnum/Rhododendron",
+                              "Treed poor fen Feathermoss/Rhododendron","Treed bog Feathermoss/Rhododendron",
+                              "Treed bog Rhododendron")~ "Present"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(sphagnum_cover = case_when(veg_detail %in% c("Treed bog Sphagnum/Rhododendron","Treed bog Sphagnum")~ "Present"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(other_moss_cover = case_when(veg_detail %in% c("Treed poor fen Feathermoss/Rhododendron","Treed bog Feathermoss/Rhododendron",
+                                                        "Treed poor fen Feathermoss", "Treed poor fen Feathermoss/Salix")~ "Present"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(sedge = case_when(veg_detail %in% c("Treed poor fen Carex/Salix","Treed poor fen Carex" )~ "Present"))
+davidson.21.monthly <- davidson.21.monthly %>% 
+  mutate(dec_broad_tree = case_when(veg_detail %in% c("Treed poor fen Carex/Salix", "Treed poor fen Feathermoss/Salix")~ "Present"))
 
 ########----------------------------------------------------------------------------------------------
 PIdat.ec <- rbindlist(list(sonnentag, pink, masa, boike, jong, sabrekov.ec, dyukarev.ec, roy, 
-                           hung.ec ), fill = TRUE)
+                           hung.ec, rautakoski ), fill = TRUE)
 PIdat.ec$extraction_source <- "User-contributed"
 PIdat.ch <- rbindlist(list(peacock, jung, althuizen,  jassey, sabrekov.ch, dyukarev.ch, hung.ch,
-                            davidson.16.monthly, davidson.19.monthly ), 
+                            davidson.16.monthly, davidson.19.monthly, davidson.21.monthly ), 
                       fill = TRUE)
 PIdat.ch$extraction_source <- "User-contributed"
 
